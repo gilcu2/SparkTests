9930fd8d8031b8e4125c4b94ebb0d0950fb3e59b
gilcu2 <gilcu2@gmail.com>
2016-12-23 19:39:37 +0100

    debug working

--- build.sbt
+++ build.sbt
@@ -4,4 +4,15 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.2"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.2" % "provided"
+
+mainClass in assembly := Some("SimpleSpark")
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case "application.conf"            => MergeStrategy.concat
+  case "reference.conf"              => MergeStrategy.concat
+  case x =>
+    val baseStrategy = (assemblyMergeStrategy in assembly).value
+    baseStrategy(x)
+}
--- src/main/scala/SimpleSpark.scala
+++ src/main/scala/SimpleSpark.scala
@@ -7,12 +7,19 @@ import org.apache.spark.SparkConf
 /**
   * Created by toddmcgrath on 6/15/16.
   */
-object SimpleScalaSpark {
+object SimpleSpark {
 
   def main(args: Array[String]) {
-    val logFile = "/opt/apache-spark//README.md" // Should be some file on your system
+
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
+
+    sc.setCheckpointDir("/tmp/spark/")
+
+    println("-------------Attach debugger now!--------------")
+    Thread.sleep(8000)
+
+    val logFile = "/opt/apache-spark//README.md" // Should be some file on your system
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
