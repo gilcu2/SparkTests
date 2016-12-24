9b828aa254a009ae55e2929aaac8bf95eeab69ec
gilcu2 <gilcu2@gmail.com>
2016-12-24 19:07:02 +0100

    count words in temporal window

--- src/main/scala/WordCountStreaming.scala
+++ src/main/scala/WordCountStreaming.scala
@@ -5,19 +5,27 @@
 import org.apache.spark._
 import org.apache.spark.streaming._
 
-object SparkStreaming {
+// To type lines
+// nc -lk 9999
+
+object WordCountStreaming {
 
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val conf = new SparkConf().setMaster("local[*]").setAppName("NetworkWordCount")
     val ssc = new StreamingContext(conf, Seconds(1))
+
+    ssc.checkpoint("/tmp/streaming")
+    val initialRDD = ssc.sparkContext.parallelize(List(("hello", 5), ("world", 1)))
 
     val lines = ssc.socketTextStream("localhost", 9999)
     val words = lines.flatMap(_.split(" "))
-    val pairs = words.map(word => (word, 1))
-    val wordCounts = pairs.reduceByKey(_ + _)
+
+    val wordCounts = words.map(x => (x, 1))
+      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(5), 2)
 
     wordCounts.print()
+
 
     ssc.start()             // Start the computation
     ssc.awaitTermination()  // Wait for the computation to terminate
